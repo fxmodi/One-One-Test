dcdd2e157daa0e5165f1675313899f26f2aaad8b
HQ\FXModi <fremil.modi@expresspros.com>
2024-04-08 17:00:30 +0530

    123

--- Test Suites/KatalonLearning_New.ts
+++ Test Suites/KatalonLearning_New.ts
@@ -11,18 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>765c5061-93e1-4319-8d50-942cea86159f</testSuiteGuid>
-   <testCaseLink>
-      <guid>7f9f5085-0d9f-4e6d-8338-6a61871dabe8</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/KatalonLearning/Amazon</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>acea0f38-31a5-4582-94b9-b4a297a3cba0</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/KatalonLearning/OrangeHRM_Failed</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
